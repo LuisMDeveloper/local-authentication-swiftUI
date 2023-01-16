c80f946a7e0382d228088765f6e48f3864e720a8
Luis Manuel Ramirez Vargas <VargasLuis2@JohnDeere.com>
2023-01-16 15:16:30 -0600

    Initial Commit

---
+++ Local Authentication/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Local Authentication
+//
+//  Created by Luis Manuel Ramirez Vargas on 16/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Local Authentication/Local_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_AuthenticationApp.swift
+//  Local Authentication
+//
+//  Created by Luis Manuel Ramirez Vargas on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Local_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
